14cb49e1b3dd3fcd908748f6b8fb7860451ca133
PHONGUIT22 <np79857@gmail.com>
2026-03-08 10:53:58 +0700

    feat: init nextjs frontend for podsphere

---
+++ podsphere-web/src/lib/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Tự động đính kèm Token nếu có (để vượt qua PremiumMiddleware của mày)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
